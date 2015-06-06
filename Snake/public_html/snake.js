19c0c978534bf829d5e84b3fbe53ec3b04ca961b
Ryan Rogers <ryan.rogers@mavs.uta.edu>
2015-06-05 19:30:00 -0500

    Added Mirror Snakes on Ring Pickup
    Additionally, added text printing for current and longest snake. After
    getting the ring, the mirror snake will do the reverse of all of yours
    moves but any powerups it gets apply to both of you, in the future the
    mirror snake make also mirror your attacks.

--- Snake/public_html/snake.js
+++ Snake/public_html/snake.js
@@ -19,19 +19,30 @@ var right = 0;
 var left = 1;
 var up = 2;
 var down = 3;
+var longestSnake = 1;
+var mirrorSnake = false;
+var increaseSnake = 0;
 
 // Images
 var eggImage = new Image();
 eggImage.src = "images/egg.png";
-var snakeImages = [];
+var ringImage = new Image();
+ringImage.src = "images/ring.png";
+var rings = [];
 var shotImage = [];
 for(image = 0; image < 4; image++) {
     shotImage[image] = new Image();
     shotImage[image].src = "images/shot" + image + ".png";
 }
+var snakeImages = [];
 for(image = 0; image < 12; image++) {
     snakeImages[image] = new Image();
     snakeImages[image].src = "images/" + image + ".png";
+}
+var invertedImages = [];
+for(image = 0; image < 12; image++) {
+    invertedImages[image] = new Image();
+    invertedImages[image].src = "inverted/" + image + ".png";
 }
 
 // Updates timeSinceLastFrame every 1000/60 ms
@@ -71,13 +82,11 @@ var orient = function(){
         canvas.width = window.innerWidth;
         canvas.height = (canvas.width / 16) * 9;
         landscape = false;
-        console.log("Set to portrait"); // DEBUG
         tileWidth = canvas.width / 64;
     } else {
         canvas.width = (window.innerHeight / 9) * 16;
         canvas.height = window.innerHeight;
         landscape = true;
-        console.log("Set to landscape"); // DEBUG
         tileWidth = canvas.width / 32;
     }
 };
@@ -112,6 +121,11 @@ var update = function() {
 // Draws the current snake
 function drawSnake() {
     // img, x, y, width, height
+    // Drawing mirror snake
+    if(mirrorSnake) {
+        drawThisSnake(mirrorSnake);
+    }
+    drawThisSnake(snake);
     // Drawing projectiles
     for(index = 0; index < projectiles.length; index++) {
         context.drawImage(shotImage[projectiles[index][2]], 
@@ -119,47 +133,90 @@ function drawSnake() {
                 projectiles[index][1] * tileWidth, 
                 tileWidth, tileWidth);
     }
-    // If snake needs more than head and tail
-    if(snake.length > 2) {
-        // Drawing body pieces
-        for(index = 1; index < snake.length - 1; index++) {
-            context.drawImage(snakeImages[8 + snake[index].direction], 
-                    snake[index][0] * tileWidth, snake[index][1] * tileWidth, 
-                    tileWidth, tileWidth);
-        }
-    }
-    // If snake needs a tail
-    if(snake.length > 1) {
-        // Drawing tail
-        context.drawImage(snakeImages[4 + snake[0].direction], 
-                    snake[0][0] * tileWidth, snake[0][1] * tileWidth, 
-                    tileWidth, tileWidth);
-    }
-    // Drawing head
-    context.drawImage(snakeImages[snake[snake.length - 1].direction], 
-                snake[snake.length - 1][0] * tileWidth, 
-                snake[snake.length - 1][1] * tileWidth, tileWidth, tileWidth);
     // Drawing powerups
     for(index = 0; index < powerups.length; index++) {
         context.drawImage(eggImage, powerups[index][0] * tileWidth, 
                 powerups[index][1] * tileWidth, tileWidth, tileWidth);
     }
+    // Drawing rings
+    for(index = 0; index < rings.length; index++) {
+        context.drawImage(ringImage, rings[index][0] * tileWidth,
+                rings[index][1] * tileWidth, tileWidth, tileWidth);
+    }
+    // Drawing text
+    context.fillStyle = "white";
+    context.font = "12px Arial";
+    context.fillText("Longest: " + longestSnake, canvas.width/100,
+            canvas.height/1.02);
+    context.fillText("Current: " + snake.length, canvas.width/10,
+            canvas.height/1.02);
 };
+
+// Draws the given snake
+function drawThisSnake(inputSnake) {
+    // If snake needs more than head and tail
+    var currentImages = snakeImages;
+    if(inputSnake === mirrorSnake) {
+        currentImages = invertedImages;
+    }
+    if(inputSnake.length > 2) {
+        // Drawing body pieces
+        for(index = 1; index < inputSnake.length - 1; index++) {
+            context.drawImage(currentImages[8 + inputSnake[index].direction], 
+                    inputSnake[index][0] * tileWidth,
+                    inputSnake[index][1] * tileWidth, 
+                    tileWidth, tileWidth);
+        }
+    }
+    // If snake needs a tail
+    if(inputSnake.length > 1) {
+        // Drawing tail
+        context.drawImage(currentImages[4 + inputSnake[0].direction], 
+                    inputSnake[0][0] * tileWidth, 
+                    inputSnake[0][1] * tileWidth, 
+                    tileWidth, tileWidth);
+    }
+    // Drawing head
+    context.drawImage(currentImages[inputSnake[inputSnake.length - 1].direction], 
+                inputSnake[inputSnake.length - 1][0] * tileWidth, 
+                inputSnake[inputSnake.length - 1][1] * tileWidth, 
+                tileWidth, tileWidth);
+    }
 
 // Keyboard press
 function keyDown(event) {
     // "W" or Up arrow
     if(event.keyCode === 87 || event.keyCode === 38) {
-        snake[snake.length - 1].direction = up;
+        if(snake[snake.length - 1].direction !== down) {
+            snake[snake.length - 1].direction = up;
+            if(mirrorSnake) {
+                mirrorSnake[mirrorSnake.length - 1].direction = down;
+            }
+        }
     // "D" or Right arrow
     } else if(event.keyCode === 68 || event.keyCode === 39) {
-        snake[snake.length - 1].direction = right;
+        if(snake[snake.length - 1].direction !== left) {
+            snake[snake.length - 1].direction = right;
+            if(mirrorSnake) {
+                mirrorSnake[mirrorSnake.length - 1].direction = left;
+            }
+        }
     // "S" or Down arrow
     } else if(event.keyCode === 83 || event.keyCode === 40) {
-        snake[snake.length - 1].direction = down;
+        if(snake[snake.length - 1].direction !== up) {
+            snake[snake.length - 1].direction = down;
+            if(mirrorSnake) {
+                mirrorSnake[mirrorSnake.length - 1].direction = up;
+            }
+        }
     // "A" or Left arrow
     } else if(event.keyCode === 65 || event.keyCode === 37) {
-        snake[snake.length - 1].direction = left;
+        if(snake[snake.length - 1].direction !== right) {
+            snake[snake.length - 1].direction = left;
+            if(mirrorSnake) {
+                mirrorSnake[mirrorSnake.length - 1].direction = right;
+            }
+        }
     // "Space" bar
     } else if(event.keyCode === 32) {
         createProjectile();
@@ -180,74 +237,119 @@ function createProjectile() {
 };
 
 // New powerup
-function createPowerup() {
-    var powerup = [];
-    powerup[0] = Math.floor((Math.random() * 64));
-    powerup[1] = Math.floor((Math.random() * 36));
-    powerups.push(powerup);
-    requestAnimationFrame(update);
+function createPowerup(type) {
+    if(type.length < 5) {
+        var powerup = [];
+        powerup[0] = Math.floor((Math.random() * 64));
+        powerup[1] = Math.floor((Math.random() * 36));
+        type.push(powerup);
+        requestAnimationFrame(update);
+    }
 };
 
-// Moves the snake along
-function snakeMove() {
+// Moving the given snake
+function moveSnake(inputSnake) {
     var snakeLink = [];
-    if(snake[snake.length - 1].direction === up) {
-        snakeLink.push(snake[snake.length - 1][0]);
-        snakeLink.push(snake[snake.length - 1][1] - 1);
+    if(inputSnake[inputSnake.length - 1].direction === up) {
+        snakeLink.push(inputSnake[inputSnake.length - 1][0]);
+        snakeLink.push(inputSnake[inputSnake.length - 1][1] - 1);
         snakeLink.direction = up;
-    } else if(snake[snake.length - 1].direction === down) {
-        snakeLink.push(snake[snake.length - 1][0]);
-        snakeLink.push(snake[snake.length - 1][1] + 1);
+    } else if(inputSnake[inputSnake.length - 1].direction === down) {
+        snakeLink.push(inputSnake[inputSnake.length - 1][0]);
+        snakeLink.push(inputSnake[inputSnake.length - 1][1] + 1);
         snakeLink.direction = down;
-    } else if(snake[snake.length - 1].direction === left) {
-        snakeLink.push(snake[snake.length - 1][0] - 1);
-        snakeLink.push(snake[snake.length - 1][1]);
+    } else if(inputSnake[inputSnake.length - 1].direction === left) {
+        snakeLink.push(inputSnake[inputSnake.length - 1][0] - 1);
+        snakeLink.push(inputSnake[inputSnake.length - 1][1]);
         snakeLink.direction = left;
     // Direction is right
     } else {
-        snakeLink.push(snake[snake.length - 1][0] + 1);
-        snakeLink.push(snake[snake.length - 1][1]);
+        snakeLink.push(inputSnake[inputSnake.length - 1][0] + 1);
+        snakeLink.push(inputSnake[inputSnake.length - 1][1]);
         snakeLink.direction = right;
     }
     // Moving snake forward
-    snake.push(snakeLink);
+    inputSnake.push(snakeLink);
     // Checking if moved outside of map
-    if(snake[snake.length - 1][0] < 0
-            || snake[snake.length - 1][0] > 63
-            || snake[snake.length - 1][1] < 0
-            || snake[snake.length - 1][1] > 35) {
-        restart();
+    if(inputSnake[inputSnake.length - 1][0] < 0
+            || inputSnake[inputSnake.length - 1][0] > 63
+            || inputSnake[inputSnake.length - 1][1] < 0
+            || inputSnake[inputSnake.length - 1][1] > 35) {
+        return false;
     // Checking if snake has eaten itself
     } else {
         var restartRequired = false;
-        if(snake.length > 3) {
-            for(index = 0; index < snake.length - 1; index++) {
-                if(snake[index][0] === snake[snake.length - 1][0]
-                        && snake[index][1] === snake[snake.length - 1][1]) {
+        if(inputSnake.length > 3) {
+            for(index = 0; index < inputSnake.length - 1; index++) {
+                if(inputSnake[index][0] === inputSnake[inputSnake.length - 1][0]
+                        && inputSnake[index][1] === inputSnake[inputSnake.length - 1][1]) {
                     restartRequired = true;
                 }
             }
         }
         if(restartRequired) {
-            restart();
+            return false;
         // Checking if powerup has been eaten
         } else {
             var powerupEaten = false;
             for(index = 0; index < powerups.length; index++) {
-                if(powerups[index][0] === snake[snake.length - 1][0]
-                        && powerups[index][1] === snake[snake.length - 1][1]) {
+                if(powerups[index][0] === inputSnake[inputSnake.length - 1][0]
+                        && powerups[index][1] 
+                        === inputSnake[inputSnake.length - 1][1]) {
                     powerups.splice(index, 1);
                     powerupEaten = true;
                 }
             }
             if(powerupEaten) {
-                createPowerup();
+                if(longestSnake < snake.length) {
+                    longestSnake = snake.length;
+                }
+                if(powerups.length < 4) {
+                    createPowerup(powerups);
+                }
+                if(inputSnake === mirrorSnake) {
+                    increaseSnake += 1;
+                    inputSnake.splice(0, 1);
+                }
             } else {
-                snake.splice(0, 1);
+                if(increaseSnake > 0) {
+                    increaseSnake -= 1;
+                } else {
+                    inputSnake.splice(0, 1);
+                }
+            }
+        }
+        // Checking if ring has been eaten
+        for(index = 0; index < rings.length; index++) {
+            if(rings[index][0] === inputSnake[inputSnake.length - 1][0]
+                    && rings[index][1] === inputSnake[inputSnake.length - 1][1]) {
+                mirrorSnake = [inputSnake.length];
+                for(index2 = 0; index2 < inputSnake.length; index2++) {
+                    mirrorSnake[index2] = inputSnake[index2].slice();
+                    mirrorSnake[index2] = inputSnake[index2].slice();
+                    mirrorSnake[index2].direction = snake[index2].direction;
+                }
+                rings.splice(index, 1);
             }
         }
     }
-    requestAnimationFrame(update);
+    return true;
+};
+
+// Moves the snake along
+function snakeMove() {
+    if(moveSnake(snake)) {
+        requestAnimationFrame(update);
+    } else {
+        restart();
+    }
+    if(mirrorSnake) {
+        if(moveSnake(mirrorSnake)) {
+            requestAnimationFrame(update);
+        } else {
+            mirrorSnake = false;
+        }
+    }
 };
 
 // Moves all projectiles
@@ -277,6 +379,7 @@ function restart() {
     snake = [[5, 5]];
     snake[0].direction = right;
     powerups = [[30, 30], [20, 10], [25, 5]];
+    rings = [[20, 20]];
 }
 
 // At load time
@@ -291,4 +394,12 @@ window.onload = function() {
     
     window.setInterval(snakeMove, 1000/10);
     window.setInterval(projectileMove, 1000/20);
+    window.setInterval(function() {
+        if(rings.length < 1) {
+            createPowerup(rings);
+            if(powerups.length < 5) {
+                createPowerup(powerups);
+            }
+        }
+    }, 10000);
 };
